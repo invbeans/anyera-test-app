f4a1e3915649aeffea38669afda9c28396a77b56
invbeans <vlada.3234.vasileva@gmail.com>
2023-11-12 16:02:56 +0400

    returned overlay

--- src/app/main/components/main-container.jsx
+++ src/app/main/components/main-container.jsx
@@ -5,9 +5,10 @@ export default function MainContainer({ children }) {
     return (
         <div className={styles["main-container"]}>
             <ImageBackground />
-
-            <div className={styles["children-container"]}>
-                {children}
+            <div className={styles["color-overlay"]}>
+                <div className={styles["children-container"]}>
+                    {children}
+                </div>
             </div>
         </div>
     )
